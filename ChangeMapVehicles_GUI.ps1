86a53b9ad45fb833954f17b3280a36ecc46eb992
vigi97la <1.le_bars@laposte.net>
2023-06-13 21:56:11 +0200

    Add new columns in vehicles_db.csv

--- ChangeMapVehicles_GUI.ps1
+++ ChangeMapVehicles_GUI.ps1
@@ -20,7 +20,7 @@ $forcedTeam2 = "EU"
 #Add config file to remember last modFolder, levelFolder,...
 
 $instructionsLabel = New-Object System.Windows.Forms.Label
-$instructionsLabel.Location = New-Object System.Drawing.Point(300,200)
+$instructionsLabel.Location = New-Object System.Drawing.Point(300,300)
 $instructionsLabel.Size = New-Object System.Drawing.Size(300,100)
 $instructionsLabel.Text =
 "Please install 7z.exe (7-Zip) and ensure it is in Windows PATH, and reboot. "+
@@ -60,7 +60,7 @@ $form.Controls.Add($gameModeLabel)
 
 $gameModeTextBox = New-Object System.Windows.Forms.TextBox
 $gameModeTextBox.Location = New-Object System.Drawing.Point(105,$offset)
-$gameModeTextBox.Size = New-Object System.Drawing.Size(260,20)
+$gameModeTextBox.Size = New-Object System.Drawing.Size(155,20)
 $gameModeTextBox.Text = $gameMode
 $form.Controls.Add($gameModeTextBox)
 
@@ -73,7 +73,7 @@ $form.Controls.Add($mapSizeLabel)
 
 $mapSizeTextBox = New-Object System.Windows.Forms.TextBox
 $mapSizeTextBox.Location = New-Object System.Drawing.Point(105,$offset)
-$mapSizeTextBox.Size = New-Object System.Drawing.Size(260,20)
+$mapSizeTextBox.Size = New-Object System.Drawing.Size(155,20)
 $mapSizeTextBox.Text = $mapSize
 $form.Controls.Add($mapSizeTextBox)
 
@@ -86,7 +86,7 @@ $form.Controls.Add($forcedTeam1Label)
 
 $forcedTeam1TextBox = New-Object System.Windows.Forms.TextBox
 $forcedTeam1TextBox.Location = New-Object System.Drawing.Point(105,$offset)
-$forcedTeam1TextBox.Size = New-Object System.Drawing.Size(260,20)
+$forcedTeam1TextBox.Size = New-Object System.Drawing.Size(155,20)
 $forcedTeam1TextBox.Text = $forcedTeam1
 $form.Controls.Add($forcedTeam1TextBox)
 
@@ -99,7 +99,7 @@ $form.Controls.Add($forcedTeam2Label)
 
 $forcedTeam2TextBox = New-Object System.Windows.Forms.TextBox
 $forcedTeam2TextBox.Location = New-Object System.Drawing.Point(105,$offset)
-$forcedTeam2TextBox.Size = New-Object System.Drawing.Size(260,20)
+$forcedTeam2TextBox.Size = New-Object System.Drawing.Size(155,20)
 $forcedTeam2TextBox.Text = $forcedTeam2
 $form.Controls.Add($forcedTeam2TextBox)
 
@@ -126,6 +126,14 @@ $bEnforcePreferredTeamsCheckbox.Size = new-object System.Drawing.Size(250,20)
 $bEnforcePreferredTeamsCheckbox.Text = "bEnforcePreferredTeams"
 $bEnforcePreferredTeamsCheckbox.Checked = $false
 $form.Controls.Add($bEnforcePreferredTeamsCheckbox)
+
+$offset += 20
+$bEnforceSizeCategoryCheckbox = new-object System.Windows.Forms.checkbox
+$bEnforceSizeCategoryCheckbox.Location = new-object System.Drawing.Size(10,$offset)
+$bEnforceSizeCategoryCheckbox.Size = new-object System.Drawing.Size(250,20)
+$bEnforceSizeCategoryCheckbox.Text = "bEnforceSizeCategory"
+$bEnforceSizeCategoryCheckbox.Checked = $false
+$form.Controls.Add($bEnforceSizeCategoryCheckbox)
 
 $offset += 20
 $bEnforceAmphibiousCheckbox = new-object System.Windows.Forms.checkbox
@@ -160,6 +168,14 @@ $bEnforceVTOLCheckbox.Checked = $true
 $form.Controls.Add($bEnforceVTOLCheckbox)
 
 $offset += 20
+$bEnforceNeedWaterCheckbox = new-object System.Windows.Forms.checkbox
+$bEnforceNeedWaterCheckbox.Location = new-object System.Drawing.Size(10,$offset)
+$bEnforceNeedWaterCheckbox.Size = new-object System.Drawing.Size(250,20)
+$bEnforceNeedWaterCheckbox.Text = "bEnforceNeedWater"
+$bEnforceNeedWaterCheckbox.Checked = $true
+$form.Controls.Add($bEnforceNeedWaterCheckbox)
+
+$offset += 20
 $bEnforceNeedAirfieldCheckbox = new-object System.Windows.Forms.checkbox
 $bEnforceNeedAirfieldCheckbox.Location = new-object System.Drawing.Size(10,$offset)
 $bEnforceNeedAirfieldCheckbox.Size = new-object System.Drawing.Size(250,20)
@@ -176,6 +192,30 @@ $bEnforceNeedLargeAirfieldCheckbox.Checked = $true
 $form.Controls.Add($bEnforceNeedLargeAirfieldCheckbox)
 
 $offset += 20
+$bEnforceHeavilyArmoredCheckbox = new-object System.Windows.Forms.checkbox
+$bEnforceHeavilyArmoredCheckbox.Location = new-object System.Drawing.Size(10,$offset)
+$bEnforceHeavilyArmoredCheckbox.Size = new-object System.Drawing.Size(250,20)
+$bEnforceHeavilyArmoredCheckbox.Text = "bEnforceHeavilyArmored"
+$bEnforceHeavilyArmoredCheckbox.Checked = $false
+$form.Controls.Add($bEnforceHeavilyArmoredCheckbox)
+
+$offset += 20
+$bEnforceHeavilyArmedCheckbox = new-object System.Windows.Forms.checkbox
+$bEnforceHeavilyArmedCheckbox.Location = new-object System.Drawing.Size(10,$offset)
+$bEnforceHeavilyArmedCheckbox.Size = new-object System.Drawing.Size(250,20)
+$bEnforceHeavilyArmedCheckbox.Text = "bEnforceHeavilyArmed"
+$bEnforceHeavilyArmedCheckbox.Checked = $false
+$form.Controls.Add($bEnforceHeavilyArmedCheckbox)
+
+$offset += 20
+$bEnforceHasManyPassengersCheckbox = new-object System.Windows.Forms.checkbox
+$bEnforceHasManyPassengersCheckbox.Location = new-object System.Drawing.Size(10,$offset)
+$bEnforceHasManyPassengersCheckbox.Size = new-object System.Drawing.Size(250,20)
+$bEnforceHasManyPassengersCheckbox.Text = "bEnforceHasManyPassengers"
+$bEnforceHasManyPassengersCheckbox.Checked = $false
+$form.Controls.Add($bEnforceHasManyPassengersCheckbox)
+
+$offset += 20
 $bEnforceCanBeAirDroppedCheckbox = new-object System.Windows.Forms.checkbox
 $bEnforceCanBeAirDroppedCheckbox.Location = new-object System.Drawing.Size(10,$offset)
 $bEnforceCanBeAirDroppedCheckbox.Size = new-object System.Drawing.Size(250,20)
@@ -183,9 +223,9 @@ $bEnforceCanBeAirDroppedCheckbox.Text = "bEnforceCanBeAirDropped"
 $bEnforceCanBeAirDroppedCheckbox.Checked = $false
 $form.Controls.Add($bEnforceCanBeAirDroppedCheckbox)
 
-$offset += 20
+$offset = 115
 $bRemoveIncompatibleCheckbox = new-object System.Windows.Forms.checkbox
-$bRemoveIncompatibleCheckbox.Location = new-object System.Drawing.Size(10,$offset)
+$bRemoveIncompatibleCheckbox.Location = new-object System.Drawing.Size(300,$offset)
 $bRemoveIncompatibleCheckbox.Size = new-object System.Drawing.Size(250,20)
 $bRemoveIncompatibleCheckbox.Text = "bRemoveIncompatible"
 $bRemoveIncompatibleCheckbox.Checked = $false
@@ -193,7 +233,7 @@ $form.Controls.Add($bRemoveIncompatibleCheckbox)
 
 $offset += 20
 $bRandomizeTeam1VehiclesCheckbox = new-object System.Windows.Forms.checkbox
-$bRandomizeTeam1VehiclesCheckbox.Location = new-object System.Drawing.Size(10,$offset)
+$bRandomizeTeam1VehiclesCheckbox.Location = new-object System.Drawing.Size(300,$offset)
 $bRandomizeTeam1VehiclesCheckbox.Size = new-object System.Drawing.Size(250,20)
 $bRandomizeTeam1VehiclesCheckbox.Text = "bRandomizeTeam1Vehicles"
 $bRandomizeTeam1VehiclesCheckbox.Checked = $true
@@ -201,7 +241,7 @@ $form.Controls.Add($bRandomizeTeam1VehiclesCheckbox)
 
 $offset += 20
 $bRandomizeTeam2VehiclesCheckbox = new-object System.Windows.Forms.checkbox
-$bRandomizeTeam2VehiclesCheckbox.Location = new-object System.Drawing.Size(10,$offset)
+$bRandomizeTeam2VehiclesCheckbox.Location = new-object System.Drawing.Size(300,$offset)
 $bRandomizeTeam2VehiclesCheckbox.Size = new-object System.Drawing.Size(250,20)
 $bRandomizeTeam2VehiclesCheckbox.Text = "bRandomizeTeam2Vehicles"
 $bRandomizeTeam2VehiclesCheckbox.Checked = $true
@@ -209,7 +249,7 @@ $form.Controls.Add($bRandomizeTeam2VehiclesCheckbox)
 
 $offset += 20
 $bUseAutoBackupCheckbox = new-object System.Windows.Forms.checkbox
-$bUseAutoBackupCheckbox.Location = new-object System.Drawing.Size(10,$offset)
+$bUseAutoBackupCheckbox.Location = new-object System.Drawing.Size(300,$offset)
 $bUseAutoBackupCheckbox.Size = new-object System.Drawing.Size(250,20)
 $bUseAutoBackupCheckbox.Text = "bUseAutoBackup"
 $bUseAutoBackupCheckbox.Checked = $true
@@ -217,11 +257,11 @@ $form.Controls.Add($bUseAutoBackupCheckbox)
 
 $offset += 20
 $applyButton = new-object System.Windows.Forms.Button
-$applyButton.Location = new-object System.Drawing.Size(500,$offset)
+$applyButton.Location = new-object System.Drawing.Size(500,400)
 $applyButton.Size = new-object System.Drawing.Size(100,40)
 $applyButton.Text = "Apply"
 $applyButton.Add_Click({
-	.\ChangeMapVehicles.ps1 $levelFolderTextBox.Text $gameModeTextBox.Text $mapSizeTextBox.Text $forcedTeam1TextBox.Text $forcedTeam2TextBox.Text $bEnforceVehicleTypeCheckbox.Checked $bEnforceCompatibleTeamsCheckbox.Checked $bEnforcePreferredTeamsCheckbox.Checked $bEnforceAmphibiousCheckbox.Checked $bEnforceFloatingCheckbox.Checked $bEnforceFlyingCheckbox.Checked $bEnforceVTOLCheckbox.Checked $bEnforceNeedAirfieldCheckbox.Checked $bEnforceNeedLargeAirfieldCheckbox.Checked $bEnforceCanBeAirDroppedCheckbox.Checked $bRemoveIncompatibleCheckbox.Checked $bRandomizeTeam1VehiclesCheckbox.Checked $bRandomizeTeam2VehiclesCheckbox.Checked $bUseAutoBackupCheckbox.Checked | Write-Host
+	.\ChangeMapVehicles.ps1 $levelFolderTextBox.Text $gameModeTextBox.Text $mapSizeTextBox.Text $forcedTeam1TextBox.Text $forcedTeam2TextBox.Text $bEnforceVehicleTypeCheckbox.Checked $bEnforceCompatibleTeamsCheckbox.Checked $bEnforcePreferredTeamsCheckbox.Checked $bEnforceSizeCategoryCheckbox.Checked $bEnforceAmphibiousCheckbox.Checked $bEnforceFloatingCheckbox.Checked $bEnforceFlyingCheckbox.Checked $bEnforceVTOLCheckbox.Checked $bEnforceNeedWaterCheckbox.Checked $bEnforceNeedAirfieldCheckbox.Checked $bEnforceNeedLargeAirfieldCheckbox.Checked $bEnforceHeavilyArmoredCheckbox.Checked $bEnforceHeavilyArmedCheckbox.Checked $bEnforceHasManyPassengersCheckbox.Checked $bEnforceCanBeAirDroppedCheckbox.Checked $bRemoveIncompatibleCheckbox.Checked $bRandomizeTeam1VehiclesCheckbox.Checked $bRandomizeTeam2VehiclesCheckbox.Checked $bUseAutoBackupCheckbox.Checked | Write-Host
 })
 $form.Controls.Add($applyButton)
 
