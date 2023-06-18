fc1a6a35d6187d89f1cead0b2392ad04c9b82426
vigi97la <1.le_bars@laposte.net>
2023-06-18 23:19:13 +0200

    Add ListVehicles_GUI

---
+++ ListVehicles.ps1
@@ -0,0 +1,82 @@
+# See also ReadMe.txt.
+
+function ListVehicles($objectsFolder, [bool]$bIncludeStationaryWeapons=$false, [bool]$bIncludeAll=$false) {
+
+	Write-Output "$objectsFolder : The following lines can be added to vehicles_db.csv as a template to be modified manually:"
+
+	Get-ChildItem "$objectsFolder\*" -R -Include "*.tweak" | ForEach-Object {
+		$file=$_.FullName
+		$bVehicle=[regex]::Match($file, "(.*Vehicles.*)",[Text.RegularExpressions.RegexOptions]"IgnoreCase, CultureInvariant").Success
+		$bStationaryWeapon=[regex]::Match($file, "(.*Weapons\\stationary.*)",[Text.RegularExpressions.RegexOptions]"IgnoreCase, CultureInvariant").Success
+		If (($bVehicle) -or ($bStationaryWeapon -and $bIncludeStationaryWeapons) -or ($bIncludeAll)) {
+			$bSkip=$false
+			$regexpr="\s*ObjectTemplate.activeSafe\s+PlayerControlObject\s+(\S+)\s*\r?\n"
+			$m=[regex]::Match(([System.IO.File]::ReadAllText($file)), $regexpr)
+			If ($m.Groups.Count -eq 2) {
+				$vehicleName=$m.Groups[1].value
+			}
+			else {
+				$bSkip=$true
+			}
+			$regexpr="\s*ObjectTemplate.vehicleHud.vehicleType\s+(\d+)\s*\r?\n"
+			$m=[regex]::Match(([System.IO.File]::ReadAllText($file)), $regexpr)
+			If ($m.Groups.Count -eq 2) {
+				$vehicleType=[int]$m.Groups[1].value
+			}
+			else {
+				$bSkip=$true
+			}
+			$regexpr="\s*ObjectTemplate.vehicleHud.hudName\s+(\S+)\s*\r?\n"
+			$m=[regex]::Match(([System.IO.File]::ReadAllText($file)), $regexpr)
+			If ($m.Groups.Count -eq 2) {
+				$vehicleHudName=$m.Groups[1].value
+			}
+			else {
+				$bSkip=$true
+			}
+			if (-not $bSkip) {
+				$bFloating=0
+				$bFlying=0
+				$bVTOL=0
+				$bNeedWater=0
+				$bNeedAirfield=0
+				$bHeavilyArmored=0
+				$bHeavilyArmed=0
+				$bHasManyPassengers=0
+				$bHasSupplies=0
+				If ($vehicleType -eq 0) {
+					$bHeavilyArmored=1
+					$bHeavilyArmed=1
+				}
+				elseif ($vehicleType -eq 1) {
+					$bHeavilyArmored=1
+					$bHeavilyArmed=1
+				}
+				elseif ($vehicleType -eq 2) {
+					$bFlying=1
+					$bVTOL=1
+				}
+				elseif ($vehicleType -eq 3) {
+					$bHasSupplies=1
+				}
+				elseif ($vehicleType -eq 4) {
+					$bFlying=1
+					$bNeedAirfield=1
+				}
+				elseif ($vehicleType -eq 5) {
+					$bHeavilyArmored=1
+				}
+				elseif ($vehicleType -eq 6) {
+					$bNeedWater=1
+				}
+
+				# Should also try to detect the number of passengers, bFloating, bAmphibious...
+				# Team sometimes could be deduced from vehicleName...?
+
+				Write-Output "$vehicleName;$vehicleType;ALL;ALL;0;;0;$bFloating;$bFlying;$bVTOL;$bNeedWater;$bNeedAirfield;0;$bHeavilyArmored;$bHeavilyArmed;$bHasManyPassengers;0;0;$bHasSupplies;$vehicleHudName;;;"
+			}
+		}
+	}
+}
+
+ListVehicles @args
---
+++ ListVehicles_GUI.ps1
@@ -0,0 +1,69 @@
+Add-Type -AssemblyName System.Windows.Forms
+
+$offset = 0
+
+$form = New-Object System.Windows.Forms.Form
+$form.width = 640
+$form.height = 480
+$form.Text = "ListVehicles"
+
+$objectsFolder = "C:\Program Files (x86)\EA GAMES\Battlefield 2\mods\bf2\Objects_server"
+#$objectsFolder = "U:\Other data\Games\Battlefield 2\Personal mods\GitHub\bf2"
+
+$instructionsLabel = New-Object System.Windows.Forms.Label
+$instructionsLabel.Location = New-Object System.Drawing.Point(300,200)
+$instructionsLabel.Size = New-Object System.Drawing.Size(300,100)
+$instructionsLabel.Text =
+"List vehicles and display lines that can be added to vehicles_db.csv as a template to be modified manually (see more info in ListVehicles.ps1)."
+$form.Controls.Add($instructionsLabel)
+
+$offset += 20
+$objectsFolderTextBox = New-Object System.Windows.Forms.TextBox
+$objectsFolderTextBox.Location = New-Object System.Drawing.Point(10,$offset)
+$objectsFolderTextBox.Size = New-Object System.Drawing.Size(480,60)
+$objectsFolderTextBox.Text = $objectsFolder
+$form.Controls.Add($objectsFolderTextBox)
+
+$changeobjectsFolderButton = new-object System.Windows.Forms.Button
+$changeobjectsFolderButton.Location = new-object System.Drawing.Size(500,$offset)
+$changeobjectsFolderButton.Size = new-object System.Drawing.Size(100,60)
+$changeobjectsFolderButton.Text = "Change Objects_server folder"
+$changeobjectsFolderButton.Add_Click({
+$folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog -Property @{
+    RootFolder            = "MyComputer"
+    Description           = "$Env:ComputerName - Select a folder"
+}
+If ($folderBrowser.ShowDialog() -eq "OK") {
+	$objectsFolder = $folderBrowser.SelectedPath
+	$objectsFolderTextBox.Text = $objectsFolder
+}
+})
+$form.Controls.Add($changeobjectsFolderButton)
+
+$offset += 20
+$bIncludeStationaryWeaponsCheckbox = new-object System.Windows.Forms.checkbox
+$bIncludeStationaryWeaponsCheckbox.Location = new-object System.Drawing.Size(10,$offset)
+$bIncludeStationaryWeaponsCheckbox.Size = new-object System.Drawing.Size(250,20)
+$bIncludeStationaryWeaponsCheckbox.Text = "bIncludeStationaryWeapons"
+$bIncludeStationaryWeaponsCheckbox.Checked = $false
+$form.Controls.Add($bIncludeStationaryWeaponsCheckbox)
+
+$offset += 20
+$bIncludeAllCheckbox = new-object System.Windows.Forms.checkbox
+$bIncludeAllCheckbox.Location = new-object System.Drawing.Size(10,$offset)
+$bIncludeAllCheckbox.Size = new-object System.Drawing.Size(250,20)
+$bIncludeAllCheckbox.Text = "bIncludeAll"
+$bIncludeAllCheckbox.Checked = $false
+$form.Controls.Add($bIncludeAllCheckbox)
+
+$offset += 20
+$applyButton = new-object System.Windows.Forms.Button
+$applyButton.Location = new-object System.Drawing.Size(500,$offset)
+$applyButton.Size = new-object System.Drawing.Size(100,40)
+$applyButton.Text = "Apply"
+$applyButton.Add_Click({
+	.\ListVehicles.ps1 $objectsFolderTextBox.Text $bIncludeStationaryWeaponsCheckbox.Checked $bIncludeAllCheckbox.Checked | Write-Host
+})
+$form.Controls.Add($applyButton)
+
+[void]$form.ShowDialog()
