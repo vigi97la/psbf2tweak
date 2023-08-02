bbc84eb48a799fdc2987df37f83fcaceb4e1d896
vigi97la <1.le_bars@laposte.net>
2023-08-02 17:35:31 +0200

    Update MiscFixes.ps1

--- MiscFixes.ps1
+++ MiscFixes.ps1
@@ -2,11 +2,9 @@
 
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 
-function FixVehicleType($objectsFolder,$outputFolder=$null,[bool]$bIncludeStationaryWeapons=$false,[bool]$bIncludeAll=$false,[bool]$bResetMapIcons=$false,[bool]$bResetHUDIcons=$false,[bool]$bResetSpottedMessage=$false,[bool]$bOverwrite=$false) {
+function FixVehicleType($objectsFolder,[bool]$bUseVehiclesDB=$true,[bool]$bIncludeStationaryWeapons=$false,[bool]$bIncludeAll=$false,[bool]$bResetMapIcons=$false,[bool]$bResetHUDIcons=$false,[bool]$bResetSpottedMessage=$true) {
 
-	If (($null -ne $outputFolder) -and ("" -ne $outputFolder)) {
-		New-Item "$outputFolder" -ItemType directory -Force | Out-Null
-	}
+	If ($bUseVehiclesDB) { $db=Import-Csv -Path "$scriptPath\vehicles_db.csv" -Delimiter ";" }
 
 	Get-ChildItem "$objectsFolder" -R -Include "*.tweak" | ForEach-Object {
 		$file=$_.FullName
@@ -20,7 +18,7 @@ function FixVehicleType($objectsFolder,$outputFolder=$null,[bool]$bIncludeStatio
 			If ($m.Groups.Count -eq 3) {
 				$vehicleName=$m.Groups[2].value
 			}
-			else {
+			Else {
 				$bSkip=$true
 			}
 
@@ -29,17 +27,57 @@ function FixVehicleType($objectsFolder,$outputFolder=$null,[bool]$bIncludeStatio
 
 				$m=[regex]::Match(([System.IO.File]::ReadAllText($file)), "\s*ObjectTemplate.vehicleHud.vehicleType\s+(\d+)\s*\r?\n")
 				If ($m.Groups.Count -eq 2) {
-					$vehicleType=[int]$m.Groups[1].value
+					$vehicleType=[double]$m.Groups[1].value
 				}
-				# VTHeavyTank (0), VTApc (1), VCHelicopter (2), car (3), VCAir (4), VTAA (5), VCSea (6)
+				# VTHeavyTank (0), VTApc (1), VCHelicopter (2), [car (3)], VCAir (4), VTAA (5), VCSea (6)
 				$m=[regex]::Match(([System.IO.File]::ReadAllText($file)), "\s*ObjectTemplate.setVehicleType\s+(\S+)\s*\r?\n")
 				If ($m.Groups.Count -eq 2) {
 					$vehicleVT=$m.Groups[1].value
 				}
-				# VCHelicopter, VCAir, VCSea...
 				$m=[regex]::Match(([System.IO.File]::ReadAllText($file)), "\s*ObjectTemplate.vehicleCategory\s+(\S+)\s*\r?\n")
 				If ($m.Groups.Count -eq 2) {
 					$vehicleVC=$m.Groups[1].value
+				}
+
+				If ($bUseVehiclesDB) {
+					$val=@($db|Where-Object {$_.vehicleName -ieq "$vehicleName"})
+					If ($val.Count -ge 1) {
+						$vehicleType=[double]$val[0].vehicleType
+						If (([double]$vehicleType -ge 0) -and ([double]$vehicleType -lt 1)) {
+							$vehicleVT="VTHeavyTank"
+							$vehicleVC=$null
+						}
+						elseIf (([double]$vehicleType -ge 1) -and ([double]$vehicleType -lt 2)) {
+							$vehicleVT="VTApc"
+							$vehicleVC=$null
+						}
+						elseIf (([double]$vehicleType -ge 2) -and ([double]$vehicleType -lt 3)) {
+							$vehicleVT=$null
+							$vehicleVC="VCHelicopter"
+						}
+						elseIf (([double]$vehicleType -ge 3) -and ([double]$vehicleType -lt 4)) {
+							$vehicleVT=$null
+							$vehicleVC=$null
+						}
+						elseIf (([double]$vehicleType -ge 4) -and ([double]$vehicleType -lt 5)) {
+							$vehicleVT=$null
+							$vehicleVC="VCAir"
+						}
+						elseIf (([double]$vehicleType -ge 5) -and ([double]$vehicleType -lt 6)) {
+							$vehicleVT="VTAA"
+							$vehicleVC=$null
+						}
+						elseIf (([double]$vehicleType -ge 6) -and ([double]$vehicleType -lt 7)) {
+							$vehicleVT=$null
+							$vehicleVC="VCSea"
+						}
+						Else {
+							Write-Warning "Unsupported vehicleType for $vehicleName in vehicles_db.csv"
+						}
+					}
+					Else {
+						Write-Warning "$vehicleName not found in vehicles_db.csv"
+					}
 				}
 
 				$sr=[System.IO.StreamReader]$file
@@ -52,9 +90,48 @@ function FixVehicleType($objectsFolder,$outputFolder=$null,[bool]$bIncludeStatio
 
 					#Write-Warning "$line"
 										
+					If ($bUseVehiclesDB) {
+						$m=[regex]::Match($line, "^\s*ObjectTemplate.vehicleHud.vehicleType\s+(\d+)\s*")
+						If ($m.Groups.Count -eq 2) {
+							$newVehicleType=[int][Math]::Floor([double]$vehicleType)
+							If ([double]$newVehicleType -ne [double]$m.Groups[1].value) {
+								$sw.WriteLine("ObjectTemplate.vehicleHud.vehicleType $newVehicleType")
+							}
+							Else {
+								$sw.WriteLine($line)
+							}
+							Continue
+						}
+						$m=[regex]::Match($line, "^\s*ObjectTemplate.setVehicleType\s+(\S+)\s*")
+						If ($m.Groups.Count -eq 2) {
+							If (($null -eq $vehicleVT) -and ($null -ne $vehicleVC)) {
+								$sw.WriteLine("ObjectTemplate.vehicleCategory $vehicleVC")
+							}
+							ElseIf (($null -ne $vehicleVT) -and ($vehicleVT -ne $m.Groups[1].value)) {
+								$sw.WriteLine("ObjectTemplate.setVehicleType $vehicleVT")
+							}
+							Else {
+								$sw.WriteLine($line)
+							}
+							Continue
+						}
+						$m=[regex]::Match($line, "^\s*ObjectTemplate.vehicleCategory\s+(\S+)\s*")
+						If ($m.Groups.Count -eq 2) {
+							If (($null -eq $vehicleVC) -and ($null -ne $vehicleVT)) {
+								$sw.WriteLine("ObjectTemplate.setVehicleType $vehicleVT")
+							}
+							ElseIf (($null -ne $vehicleVC) -and ($vehicleVC -ne $m.Groups[1].value)) {
+								$sw.WriteLine("ObjectTemplate.vehicleCategory $vehicleVC")
+							}
+							Else {
+								$sw.WriteLine($line)
+							}
+							Continue
+						}
+					}
 					If ($bResetMapIcons) {
 
-						# Should use vehicles_db.csv, see xpak_ailraider and others with specific icons, should not change if the file is found...
+						# See xpak_ailraider and others with specific icons, should not change if the file is found...
 
 						$m=[regex]::Match($line, "^\s*ObjectTemplate.vehicleHud.typeIcon\s+(\S+)\s*")
 						If ($m.Groups.Count -eq 2) {
@@ -160,23 +237,8 @@ function FixVehicleType($objectsFolder,$outputFolder=$null,[bool]$bIncludeStatio
 				}
 				$sw.close()
 				$sr.close()
-				If (($null -ne $outputFolder) -and ("" -ne $outputFolder)) {
-					# Should remove $objectsFolder from the path in $file and add the rest at the end of the desired folder...
-					$regesc=[regex]::Escape($objectsFolder)
-					$m=[regex]::Match($file, "$regesc\\(.+)",[Text.RegularExpressions.RegexOptions]"IgnoreCase, CultureInvariant")
-					$restOfPath=$m.Groups[1].value
-					#"$restOfPath"
-					$outputFile=[System.IO.Path]::Combine($outputFolder,$restOfPath)
-					#"$outputFile"
-					New-Item ([System.IO.FileInfo]$outputFile).DirectoryName -ItemType directory -Force | Out-Null
-					Move-Item -Path "$file.tmp" -Destination $outputFile -Force
-				}
-				If ($bOverwrite) { 
-					If (-not (Test-Path -Path "$file.bak")) { 
-						Move-Item -Path $file -Destination "$file.bak" -Force
-					}
-					Move-Item -Path "$file.tmp" -Destination $file -Force
-				}
+
+				Move-Item -Path "$file.tmp" -Destination $file -Force
 			}
 		}
 	}
