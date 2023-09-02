2e27d080758cf56a486bb033900e5cfa8acb7c7c
vigi97la <1.le_bars@laposte.net>
2023-09-03 01:21:18 +0200

    Update VehicleExtractor.cs

--- VehicleExtractor.cs
+++ VehicleExtractor.cs
@@ -2,9 +2,276 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 public class VehicleExtractor
 {
+    #region NOT TESTED
+    public static string ReadConFile(string file)
+    {
+        if (!File.Exists(file))
+        {
+            Console.WriteLine(file + " not found");
+            return "";
+        }
+        return File.ReadAllText(file);
+    }
+
+    public static string ConvertBlockCommentsConContent(string concontent)
+    {
+        string content = "";
+        string beginremregexpr = "^\\s*beginrem\\s*";
+        string endremregexpr = "^\\s*endrem\\s*";
+        bool bInsideBlockComment = false;
+        string[] lines = Regex.Split(concontent, "\\r?\\n");
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string line = lines[i];
+            if (bInsideBlockComment)
+            {
+                Match m = Regex.Match(line, endremregexpr, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+                if (m.Success)
+                {
+                    bInsideBlockComment = false;
+                    content += "rem " + line + "\\r\\n";
+                    continue;
+                }
+                content += "rem " + line + "\\r\\n";
+                continue;
+            }
+            Match m2 = Regex.Match(line, beginremregexpr, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+            if (m2.Success)
+            {
+                bInsideBlockComment = true;
+                content += "rem " + line + "\\r\\n";
+                continue;
+            }
+            content += line + "\\r\\n";
+        }
+        return content;
+    }
+
+    public static string cbInsideCommentLine(string concontent, int i, string line, string params1)
+    {
+        string content = params1;
+        return content;
+    }
+
+    public static string cbOutsideCommentLine(string concontent, int i, string line, string params1)
+    {
+        string content = params1;
+        content += line + "\\r\\n";
+        return content;
+    }
+
+    public static string PreProcessCommentsConContent(string concontent, Func<string, int, string, string, string> cbInsideCommentLine, Func<string, int, string, string, string> cbOutsideCommentLine)
+    {
+        string content = "";
+        string remregexpr = "^\\s*rem\\s+(.*)\\s*";
+        string beginremregexpr = "^\\s*beginrem\\s*";
+        string endremregexpr = "^\\s*endrem\\s*";
+        bool bInsideBlockComment = false;
+        string[] lines = Regex.Split(concontent, "\\r?\\n");
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string line = lines[i];
+            if (bInsideBlockComment)
+            {
+                Match m = Regex.Match(line, endremregexpr, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+                if (m.Success)
+                {
+                    bInsideBlockComment = false;
+
+                    content = cbInsideCommentLine(concontent, i, line, content);
+
+                    continue;
+                }
+
+                content = cbInsideCommentLine(concontent, i, line, content);
+
+                continue;
+            }
+            Match m2 = Regex.Match(line, remregexpr, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+            if (m2.Success)
+            {
+
+                content = cbInsideCommentLine(concontent, i, line, content);
+
+                continue;
+            }
+            Match m3 = Regex.Match(line, beginremregexpr, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+            if (m3.Success)
+            {
+                bInsideBlockComment = true;
+
+                content = cbInsideCommentLine(concontent, i, line, content);
+
+                continue;
+            }
+
+            content = cbOutsideCommentLine(concontent, i, line, content);
+
+        }
+        return content;
+    }
+
+    public static string cbConditionCode(string concontent, int i, string line, string params1)
+    {
+        string content = params1;
+        return content;
+    }
+
+    public static string cbOtherCode(string concontent, int i, string line, string params1)
+    {
+        string content = params1;
+        content += line + "\\r\\n";
+        return content;
+    }
+
+    public static string cbInsideTrueCondition(string concontent, int i, string line, string params1)
+    {
+        string content = params1;
+        content += line + "\\r\\n";
+        return content;
+    }
+
+    public static string cbInsideFalseCondition(string concontent, int i, string line, string params1)
+    {
+        string content = params1;
+        return content;
+    }
+
+
+
+    public static string PreProcessArgsConContent(string concontent, string v_arg1, string v_arg2, string v_arg3, string v_arg4, string v_arg5, string v_arg6, string v_arg7, string v_arg8, string v_arg9)
+    {
+        string content = "";
+        foreach (string line in Regex.Split(concontent, "\\r?\\n"))
+        {
+            if ((v_arg1 != null) && (v_arg1 != "")) { line.Replace("v_arg1", v_arg1); } else { line.Replace("v_arg1", "\"null\""); }
+            if ((v_arg2 != null) && (v_arg2 != "")) { line.Replace("v_arg2", v_arg2); } else { line.Replace("v_arg2", "\"null\""); }
+            if ((v_arg3 != null) && (v_arg3 != "")) { line.Replace("v_arg3", v_arg3); } else { line.Replace("v_arg3", "\"null\""); }
+            if ((v_arg4 != null) && (v_arg4 != "")) { line.Replace("v_arg4", v_arg4); } else { line.Replace("v_arg4", "\"null\""); }
+            if ((v_arg5 != null) && (v_arg5 != "")) { line.Replace("v_arg5", v_arg5); } else { line.Replace("v_arg5", "\"null\""); }
+            if ((v_arg6 != null) && (v_arg6 != "")) { line.Replace("v_arg6", v_arg6); } else { line.Replace("v_arg6", "\"null\""); }
+            if ((v_arg7 != null) && (v_arg7 != "")) { line.Replace("v_arg7", v_arg7); } else { line.Replace("v_arg7", "\"null\""); }
+            if ((v_arg8 != null) && (v_arg8 != "")) { line.Replace("v_arg8", v_arg8); } else { line.Replace("v_arg8", "\"null\""); }
+            if ((v_arg9 != null) && (v_arg9 != "")) { line.Replace("v_arg9", v_arg9); } else { line.Replace("v_arg9", "\"null\""); }
+
+            content += line + "\\r\\n";
+        }
+        return content;
+    }
+
+    public static string PreProcessConstsConContent(string concontent)
+    {
+        string[] constNames = null;
+        string[] constValues = null;
+
+        string content = "";
+        foreach (string line in Regex.Split(concontent, "\\r?\\n"))
+        {
+            Match m = Regex.Match(line, "^\\s*const\\s+(\\S+)\\s*=\\s*(\\S+)\\s*", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+            if (m.Groups.Count == 3)
+            {
+                Array.Resize(ref constNames, constNames.Length + 1);
+                constNames[constNames.Length - 1] = m.Groups[1].Value;
+                Array.Resize(ref constValues, constValues.Length + 1);
+                constValues[constValues.Length - 1] = m.Groups[2].Value;
+            }
+            else if (constNames != null)
+            {
+                for (int i = 0; i < constNames.Length; i++)
+                {
+                    string regescconstName = Regex.Escape(constNames[i]);
+                    line.Replace(regescconstName, constValues[i]);
+                }
+            }
+
+            content += line + "\\r\\n";
+        }
+        return content;
+    }
+
+    public static string PreProcessVarsConContent(string concontent)
+    {
+        string[] varNames = null;
+        string[] varValues = null;
+        bool[] bVarArrowAssignments = null;
+
+        string content = "";
+        foreach (string line in Regex.Split(concontent, "\\r?\\n"))
+        {
+            Match m = Regex.Match(line, "^\\s*var\\s+(\\S+)\\s*=\\s*(\\S+)\\s*", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+            if (m.Groups.Count == 3)
+            {
+                Array.Resize(ref varNames, varNames.Length + 1);
+                varNames[varNames.Length - 1] = m.Groups[1].Value;
+                Array.Resize(ref varValues, varValues.Length + 1);
+                varValues[varValues.Length - 1] = m.Groups[2].Value;
+                Array.Resize(ref bVarArrowAssignments, bVarArrowAssignments.Length + 1);
+                bVarArrowAssignments[bVarArrowAssignments.Length - 1] = false;
+            }
+            else if (varNames != null)
+            {
+                for (int i = 0; i < varNames.Length; i++)
+                {
+                    string regescvarName = Regex.Escape(varNames[i]);
+                    m = Regex.Match(line, "^\\s*" + regescvarName + "\\s*=\\s*(\\S+)\\s*", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+                    if (m.Groups.Count == 2)
+                    {
+                        varValues[i] = m.Groups[1].Value;
+                        bVarArrowAssignments[i] = false;
+                    }
+                    else
+                    {
+                        m = Regex.Match(line, "\\s*->\\s*" + regescvarName + "\\s*", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+                        if (m.Groups.Count == 2)
+                        {
+                            bVarArrowAssignments[i] = true;
+                        }
+                        else if (!bVarArrowAssignments[i])
+                        {
+                            line.Replace(regescvarName, varValues[i]);
+                        }
+                    }
+                }
+            }
+
+            content += line + "\\r\\n";
+        }
+        return content;
+    }
+
+    public static string GetFileIncludedConLine(string line, string file)
+    {
+        Match m =
+           Regex.Match(line,
+                       "^\\s*include\\s+(\\S+)(\\s+)?(\\S+)?(\\s+)?(\\S+)?(\\s+)?(\\S+)?(\\s+)?(\\S+)?(\\s+)?(\\S+)?(\\s+)?(\\S+)?(\\s+)?(\\S+)?(\\s+)?(\\S+)?(\\s+)?(\\S+)?",
+                       RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+        if (m.Groups.Count >= 2)
+        {
+            return Path.Combine(Path.GetDirectoryName(file), m.Groups[1].Value.Replace("\"", "").Replace("/", "\\"));
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public static string GetFileRunConLine(string line, string file)
+    {
+        Match m = Regex.Match(line, "^\\s*run\\s+(\\S+)(\\s+)?(\\S+)?(\\s+)?(\\S+)?(\\s+)?(\\S+)?(\\s+)?(\\S+)?(\\s+)?(\\S+)?(\\s+)?(\\S+)?(\\s+)?(\\S+)?(\\s+)?(\\S+)?(\\s+)?(\\S+)?", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+        if (m.Groups.Count >= 2)
+        {
+            return Path.Combine(Path.GetDirectoryName(file), m.Groups[1].Value.Replace("\"", "").Replace("/", "\\"));
+        }
+        else
+        {
+            return null;
+        }
+    }
+    #endregion NOT TESTED
     public static void MergeTemplateMultipleDefinitions(string extractedFolder, bool bShowOutput = true, bool bShowExtendedOutput = false)
     {
         if (string.IsNullOrEmpty(extractedFolder) || !Directory.Exists(extractedFolder))
